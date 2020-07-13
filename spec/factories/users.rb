dd6d8a972924a49d69ad962f9e2ee8b2798afdb8
nyantech <nyantech14@gmail.com>
2020-07-13 14:46:41 +0900

    init user model
    Signed-off-by: nyantech <nyantech14@gmail.com>

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    provider { "MyString" }
+    uid { "MyString" }
+    nickname { "MyString" }
+    name { "MyString" }
+    image_url { "MyString" }
+    description { "MyString" }
+  end
+end
