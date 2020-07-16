07f41e7351204064134e4830406e5f41f2a9996e
nyantech <nyantech14@gmail.com>
2020-07-16 18:23:54 +0900

    routes setting
    Signed-off-by: nyantech <nyantech14@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+
   root 'tweet_readers#index'
+  get 'tweet_readers/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
