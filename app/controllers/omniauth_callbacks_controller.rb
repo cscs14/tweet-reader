65cdd29057f967d0051b732eb18ef6ac88d1ece9
nyantech <nyantech14@gmail.com>
2020-07-17 17:57:47 +0900

    fix  succession omniauth_callbacks_controller
    Signed-off-by: nyantech <nyantech14@gmail.com>

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
 
