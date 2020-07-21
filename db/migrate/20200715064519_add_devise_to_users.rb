9e892673a03341a46f4a75a13c4acf78d216f134
nyantech <nyantech14@gmail.com>
2020-07-21 16:49:35 +0900

    fix table column
    Signed-off-by: nyantech <nyantech14@gmail.com>

--- db/migrate/20200715064519_add_devise_to_users.rb
+++ db/migrate/20200715064519_add_devise_to_users.rb
@@ -5,6 +5,10 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string   :nickname
 
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
       ## Rememberable
       t.datetime :remember_created_at
 
